56420e3d734a8816af463151738092ad4b7cdb47
Ferdinand <9981ferdinand@gmail.com>
2024-06-15 02:02:01 +0700

    Reset Password

--- app/src/main/java/com/capstone/signora/ForgetActivity.kt
+++ app/src/main/java/com/capstone/signora/ForgetActivity.kt
@@ -1,17 +1,46 @@
 package com.capstone.signora
 
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
 
 class ForgetActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forget)
 
+        auth = FirebaseAuth.getInstance()
 
+        val emailEditText: EditText = findViewById(R.id.email)
+        val resetButton: Button = findViewById(R.id.ganti_button)
+
+        resetButton.setOnClickListener {
+            val email = emailEditText.text.toString().trim()
+            if (email.isEmpty()) {
+                Toast.makeText(this, "Tolong masukan emailnya", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            auth.sendPasswordResetEmail(email)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(this, "Silahkan cek email anda", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(
+                            this,
+                            "Error: ${task.exception?.message}",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+        }
     }
 }
